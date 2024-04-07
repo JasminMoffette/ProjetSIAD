7c9267309029d2bc01fb263db369103c775055b2
Jasmin Moffette <Jasmin.moffette@icloud.com>
2024-04-07 12:34:26 -0400

    formatage des données pour la résolution 1 (problem1)

--- lecture_fichier.py
+++ lecture_fichier.py
@@ -7,6 +7,16 @@ class LectureFichier():
         self.produits = []
         self.operateurs = 0
         self.employes = 0
+        self.journee = 0
+        self.dict_cadence = {}
+        self.dict_employes = {}
+        self.dict_operateurs = {}
+        self.setProduits()
+        self.setOperateurs()
+        self.setEmployes()
+        self.setDictCadence()
+        self.setDictEmployes()
+        self.setDictOperateurs()
 
     def __str__(self) -> str:
         return f"{self.produits}, {self.operateurs}, {self.employes}"
@@ -23,8 +33,9 @@ class LectureFichier():
             ligne = df_produits["Ligne"][index]
             limite = df_produits["Limite de séchage"][index]
             quantite = df_produits["Quantité"][index]
+            gain = df_produits["Gain"][index]
 
-            produit_ajout = produit.Produit(lot, type, ligne, limite, quantite)
+            produit_ajout = produit.Produit(lot, type, ligne, limite, quantite, gain)
             self.produits.append(produit_ajout)
 
     def setOperateurs(self):
@@ -41,10 +52,42 @@ class LectureFichier():
           df_employes = pd.read_excel('etat.xlsx', sheet_name= "infos" ,usecols= [1,1])
 
           #Ajuter le nombre d'employé
-          self.employes = df_employes["employés"][0]     
+          self.employes = df_employes["employés"][0]
+
+    def setJournee(self):
+
+        #récupérer le temps d'une journée
+        df_journee =  pd.read_excel('etat.xlsx', sheet_name= "infos" ,usecols= [2,2])
+
+        #Ajuter le nombre d'employé
+        self.journee = df_journee["Temps journée"][0]
+
+    def setDictCadence(self):
+
+        #recupérer les cadences pour chaque ligne
+        df_cadence = pd.read_excel('etat.xlsx', sheet_name= "lignes" ,usecols= [0,1])
+
+        #générer le dictionnaire de combinaisons
+        for index in df_cadence.index:
+            self.dict_cadence[df_cadence["nom"][index]] = df_cadence["cadence"][index]
+    
+    def setDictEmployes(self):
+       
+        #recupérer le nombre d'employe pour chaque ligne
+        df_employes = pd.read_excel('etat.xlsx', sheet_name= "lignes" ,usecols= [0,2])
+
+        #générer le dictionnaire de combinaisons
+        for index in df_employes.index:
+            self.dict_employes[df_employes["nom"][index]] = df_employes["nombre d'employé"][index]
+
+    def setDictOperateurs(self):
+
+        #recupérer le nombre d'employe pour chaque ligne
+        df_operateurs = pd.read_excel('etat.xlsx', sheet_name= "lignes" ,usecols= [0,3])
+
+        #générer le dictionnaire de combinaisons
+        for index in df_operateurs.index:
+            self.dict_operateurs[df_operateurs["nom"][index]] = df_operateurs["nombre d'opérateur"][index]
+        
 
 fichier = LectureFichier()
-fichier.setProduits()
-fichier.setOperateurs()
-fichier.setEmployes()
-print(fichier)
--- produit.py
+++ produit.py
@@ -1,12 +1,13 @@
 
 class Produit():
 
-    def __init__(self, lot, type, ligne, priorite, quantite):
+    def __init__(self, lot, type, ligne, priorite, quantite, gain):
         self.lot = lot
         self.type = type
         self.ligne = ligne
         self.priorite = priorite
         self.quantite = quantite
+        self.gain = gain
 
     def __str__(self):
         return f"{self.lot}, {self.type}, {self.ligne}, {self.priorite}, {self.quantite}"
